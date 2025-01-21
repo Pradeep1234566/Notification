eb2d2598bcb565c28c4c0d25b5b0a28181bb351d
Pradeep <pawarpradeep1707@gmail.com>
2025-01-21 21:38:41 +0530

    Done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:notification/services/notification.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final notificationService = NotificationService();
+
     return Scaffold(
-      
+      appBar: AppBar(
+        title: const Text('Notification Test'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            notificationService.showNotification(
+              title: 'Test Notification',
+              body: 'This is a sample notification to test your setup.',
+            );
+          },
+          child: const Text('Show Notification'),
+        ),
+      ),
     );
   }
 }
